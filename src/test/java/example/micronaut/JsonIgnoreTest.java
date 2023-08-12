28169c795c2fea4968caa7aaee7b54ff8d5c3b77
Rune Nielsen <rune.nielsen@cellpointdigital.com>
2023-08-12 22:08:49 +0200

    first commit

---
+++ src/test/java/example/micronaut/JsonIgnoreTest.java
@@ -0,0 +1,54 @@
+package example.micronaut;
+
+import io.micronaut.serde.ObjectMapper;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+@MicronautTest
+class JsonIgnoreTest {
+
+    private final ObjectMapper objectMapper;
+
+    public JsonIgnoreTest(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Test
+    void testJacksonSerializationWithJsonIgnore() throws IOException {
+        var testModel = new TestModel();
+        testModel.setTestProperty1("testValue1");
+        testModel.setTestProperty2("testValue2");
+        var testModelStr = new com.fasterxml.jackson.databind.ObjectMapper().writeValueAsString(testModel);
+
+        Assertions.assertEquals("{}", testModelStr);
+    }
+
+    @Test
+    void testMicronautSerializationWithJsonIgnore() throws IOException {
+        var testModel = new TestModel();
+        testModel.setTestProperty1("testValue1");
+        testModel.setTestProperty2("testValue2");
+        var testModelStr = objectMapper.writeValueAsString(testModel);
+
+        Assertions.assertEquals("{}", testModelStr);
+    }
+
+    @Test
+    void testJacksonDeserializationWithJsonIgnore() throws IOException {
+        var testModel = new com.fasterxml.jackson.databind.ObjectMapper().readValue(
+                "{\"testProperty1\":\"testValue1\",\"testProperty2\":\"testValue2\"}", TestModel.class);
+
+        Assertions.assertEquals("TestModel{testProperty1='null', testProperty2='null'}", testModel.toString());
+    }
+
+    @Test
+    void testMicronautDeserializationWithJsonIgnore() throws IOException {
+        var testModel = objectMapper.readValue(
+                "{\"testProperty1\":\"testValue1\",\"testProperty2\":\"testValue2\"}", TestModel.class);
+
+        Assertions.assertEquals("TestModel{testProperty1='null', testProperty2='null'}", testModel.toString());
+    }
+}
---
+++ src/test/java/example/micronaut/TestModel.java
@@ -0,0 +1,43 @@
+package example.micronaut;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.serde.annotation.Serdeable;
+
+@JsonIgnoreProperties(
+        value = "testProperty2"
+)
+@Introspected
+@Serdeable
+public class TestModel {
+
+    @JsonIgnore
+    private String testProperty1;
+
+    private String testProperty2;
+
+    public String getTestProperty1() {
+        return testProperty1;
+    }
+
+    public void setTestProperty1(String testProperty1) {
+        this.testProperty1 = testProperty1;
+    }
+
+    public String getTestProperty2() {
+        return testProperty2;
+    }
+
+    public void setTestProperty2(String testProperty2) {
+        this.testProperty2 = testProperty2;
+    }
+
+    @Override
+    public String toString() {
+        return "TestModel{" +
+                "testProperty1='" + testProperty1 + '\'' +
+                ", testProperty2='" + testProperty2 + '\'' +
+                '}';
+    }
+}
