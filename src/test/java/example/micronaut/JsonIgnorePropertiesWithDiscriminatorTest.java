f11ab434347dc53547844f6d120209a910345922
Rune Nielsen <rune.nielsen@cellpointdigital.com>
2023-08-14 12:35:45 +0200

    minor change

--- src/test/java/example/micronaut/JsonIgnorePropertiesWithDiscriminatorTest.java
+++ src/test/java/example/micronaut/JsonIgnorePropertiesWithDiscriminatorTest.java
@@ -18,6 +18,8 @@ class JsonIgnorePropertiesWithDiscriminatorTest {
 
     @Test
     void testJacksonSerializationWithJsonIgnoreProperties() throws IOException {
+        // It is intentional for the purpose of the test, that I have set the discriminator property to something that
+        // doesn't match the class of the bean here. The value shouldn't matter, since it should be ignored.
         var testModel = new BasicBookInfo("Michael Ende", "The Neverending Story", BookInfo.TypeEnum.DETAILED);
 
         var testModelStr = new com.fasterxml.jackson.databind.ObjectMapper().writeValueAsString(testModel);
@@ -27,6 +29,8 @@ class JsonIgnorePropertiesWithDiscriminatorTest {
 
     @Test
     void testMicronautSerializationWithJsonIgnoreProperties() throws IOException {
+        // It is intentional for the purpose of the test, that I have set the discriminator property to something that
+        // doesn't match the class of the bean here. The value shouldn't matter, since it should be ignored.
         var testModel = new BasicBookInfo("Michael Ende", "The Neverending Story", BookInfo.TypeEnum.DETAILED);
 
         var testModelStr = objectMapper.writeValueAsString(testModel);
